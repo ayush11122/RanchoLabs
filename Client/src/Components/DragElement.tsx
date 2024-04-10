e6f16611d40cca502c7b03283c941949cc3d33af
ayush11122 <ad.ayushsrivastava11122@gmail.com>
2024-04-11 01:49:17 +0530

    overflow set

--- Client/src/Components/DragElement.tsx
+++ Client/src/Components/DragElement.tsx
@@ -10,7 +10,7 @@ export default function DragElement() {
     <Droppable droppableId="DragElement">
       {(provided) => (
         <div
-          className="grid lg:grid-cols-3 grid-cols-2 sm:w-4/12 w-3/4 justify-center rounded-2xl border-2 border-gray-200 mt-16 p-6  gap-4 shadow-xl "
+          className="grid lg:grid-cols-3 grid-cols-2 sm:w-4/12 w-3/4 justify-center rounded-2xl border-2 border-gray-200 mt-16 p-6 gap-4 shadow-xl overflow-auto"
           ref={provided.innerRef}
           {...provided.droppableProps}
         >
